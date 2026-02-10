7f7ae07e2944c68742247a49c4ac3256f24695bb
guilherme <guibferrarezi@gmail.com>
2026-02-10 19:00:16 -0300

    rotas arrumadas

--- controller/controller_login.js
+++ controller/controller_login.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
-
+import { usuario } from '../models/usuarios.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+
 
 const router = express.Router()
 
@@ -16,6 +17,10 @@ router.post('/', (req, res) => {
   const { usuario, senha } = req.body
   console.log('login:', usuario, senha)
   res.status(200).send(`Login recebido: ${usuario}, ${senha}`)
+})
+
+router.get("/", (req, res) => {
+  res.send(`Usuarios cadastrados: ${usuario}`)
 })
 
 export default router
